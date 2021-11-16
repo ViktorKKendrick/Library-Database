d432c58eb17ee3083d016215d68df3800efabe7a
viktor.kendrick <viktor.kendrick@gmail.com>
2021-11-16 09:44:58 -0500

    commit

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthorsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BooksController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +15,18 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api') -> prefix('v1')->group(function(){
+    Route::get('/user', function(Request $request){
+        return $request ->user();
+    });
+    // Route::get('/authors/{author}', [AuthorsController::class,
+    //  'show']);
+    // Route::get('/authors', [AuthorsController::class,
+    //  'index']);
+     Route::apiResource('/authors', AuthorsController::class);
+     Route::apiResource('/books', BooksController::class);
 });
+Route::get('/test', function(Request $request){
+    return 'Authenticated';
+});
+
